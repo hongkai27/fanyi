a564f9dbfc3214400d3ab9edee1f202a175483ee
hongkai <952817116@qq.com>
2019-03-22 16:57:54 +0800

    fanyi

---
+++ fanyi/pages/change/change.js
@@ -0,0 +1,19 @@
+//logs.js
+const util = require('../../utils/util.js')
+const app = getApp()
+Page({
+  data: {
+    curLang: {},
+    langList: app.globalData.langList
+  },
+  onShow: function () {
+    this.setData({ curLang: app.globalData.curLang })
+  },
+  onTapItem: function (e) {
+    let langObj = e.currentTarget.dataset
+    wx.setStorageSync('language', langObj)
+    this.setData({ 'curLang': langObj })
+    app.globalData.curLang = langObj
+    wx.switchTab({ url: '/pages/index/index' })
+  }
+})
